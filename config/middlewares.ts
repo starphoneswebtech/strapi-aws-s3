b629396a13e458d58ca91348bb4c5dc656e1bb2f
webtech@starphones.com.au <webtech@starphones.com.au>
2025-02-27 11:58:13 +1100

    update config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,4 +1,30 @@
 export default [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default () => ({
+    upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            credentials: {
+              accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+              secretAccessKey: process.env.AWS_ACCESS_SECRET,
+            },
+            region: process.env.AWS_REGION,
+            baseUrl: `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET}`, // This line sets the custom url format
+            params: {
+              ACL: process.env.AWS_ACL || 'public-read',
+              signedUrlExpires: process.env.AWS_SIGNED_URL_EXPIRES || 15 * 60,
+              Bucket: process.env.AWS_BUCKET,
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      }
+});

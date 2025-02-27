334a3604e337e0d7786568a0303051c7a592ab88
webtech@starphones.com.au <webtech@starphones.com.au>
2025-02-27 12:44:07 +1100

    update config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,27 +1,25 @@
 export default () => ({
     upload: {
-        s3Options: {
-            config: {
-              provider: 'aws-s3',
-              providerOptions: {
-                credentials: {
-                  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                  secretAccessKey: process.env.AWS_ACCESS_SECRET,
-                },
-                region: process.env.AWS_REGION,
-                baseUrl: `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET_NAME}`, // This line sets the custom url format
-                params: {
-                  ACL: process.env.AWS_ACL || 'public-read',
-                  signedUrlExpires: process.env.AWS_SIGNED_URL_EXPIRES || 15 * 60,
-                  Bucket: process.env.AWS_BUCKET_NAME,
-                },
-              },
-              actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-              },
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_ACCESS_SECRET,
             },
-        }
+            region: process.env.AWS_REGION,
+            baseUrl: `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET_NAME}`, // This line sets the custom url format
+            params: {
+                ACL: process.env.AWS_ACL || 'public-read',
+                signedUrlExpires: process.env.AWS_SIGNED_URL_EXPIRES || 15 * 60,
+                Bucket: process.env.AWS_BUCKET_NAME,
+            },
+            },
+            actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+            },
+        },
     }
 });
